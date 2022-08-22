2305be3f3e92dc01665c3061d2733ff3e43b2968
Joris Monleon <monleon.joris@gmail.com>
2022-08-22 17:29:07 +0200

    Maj UI + rework houshing component

--- kasa/src/features/housing/Housing.jsx
+++ kasa/src/features/housing/Housing.jsx
@@ -1,39 +1,20 @@
 import './housing.scss';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useState } from 'react';
-import arrowDown from '../../assets/arrowDown.png';
-import arrowUp from '../../assets/arrowUp.png';
+
 import dataMocked from '../../mock/dataMocked.json';
 import RatingStar from '../ratingStar/RatingStar';
 import Gallery from '../gallery/Gallery';
 import DropDown from '../dropDown/DropDown';
 
-// import { useEffect } from 'react';
-
 
 const Housing = () => {
-  // const [arrowIconDescribe, setArrowIconDescribe] = useState(false);
-  // const [arrowIconTools, setArrowIconTools] = useState(false);
-  // const [theHouse, setTheHouse] = useState({});
+  // const navigate = useNavigate();
 
   let params = useParams();
   const theHouse = dataMocked.find(house => house.id === params.id);
 
-  // useEffect(() => {
-  //   setTheHouse(houseFound);
-  // })
- 
-  // const handleVisible = (domId) => {
-  //   const content =  document.getElementById(domId);
-
-  //   if (content.classList.contains('hiddenHousing')){
-  //     content.classList.replace('hiddenHousing', 'visibleHousing');
-  //     domId === 'describeContent' ? setArrowIconDescribe(true) : setArrowIconTools(true);
-  //   } else {
-  //     content.classList.replace('visibleHousing', 'hiddenHousing');
-  //     domId === 'describeContent' ? setArrowIconDescribe(false) : setArrowIconTools(false);
-  //   }
-  // }
+  console.log(theHouse);
 
   return (
     <>
