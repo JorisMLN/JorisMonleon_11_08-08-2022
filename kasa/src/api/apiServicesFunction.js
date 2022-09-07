adc46dcfcb75899f9eb9f7f26497f08cbf3f6800
Joris Monleon <monleon.joris@gmail.com>
2022-09-07 13:50:05 +0200

    Api manager

---
+++ kasa/src/api/apiServicesFunction.js
@@ -0,0 +1,27 @@
+import dataMocked from '../mock/dataMocked.json';
+import valuesMocked from '../mock/valuesMocked.json';
+
+export const getDataToCard = () => {
+  const dataCard = dataMocked;
+  return dataCard;
+};
+
+export const getDataToLocation = () => {
+  const dataHouse = dataMocked;
+  return dataHouse;
+}
+
+export const getImages = () => {
+  const images = dataMocked;
+  return images;
+}
+
+export const getInfoAndTools = () => {
+  const infosAndTools = dataMocked;
+  return infosAndTools;
+}
+
+export const getAboutValues = () => {
+  const dataValues = valuesMocked;
+  return dataValues;
+}
